1e8210c06b35ebf30bc2249d62c6645dbccbf073
Flor Tello <flor.26071994@gmail.com>
2017-06-30 18:56:12 -0500

    Agreandocpuero

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-app.set(‘port’, (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 
 app.use('/static',express.static(path.join(__dirname, 'node_modules')));
 app.use('/static',express.static(path.join(__dirname, 'public/assets')));
@@ -11,5 +11,5 @@ app.get('/',(req,res) => {
 
 
 app.listen(app.get('port'), function() {
-  console.log(‘Node app is running on port’, app.get(‘port’));
+  console.log('Node app is running on port', app.get('port'));
 });
